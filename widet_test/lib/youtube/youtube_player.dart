346113690f8cbf629d6b712e0bc4fe10d0c5c18a
UwatakiYuichi <yuichi.uwataki@lanway.jp>
2023-04-16 07:14:25 +0900

    youtubeの機能用を簡易追加

---
+++ widet_test/lib/youtube/youtube_player.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class YoutubePlayer extends StatefulWidget {
+  const YoutubePlayer({super.key});
+
+  @override
+  State<YoutubePlayer> createState() => YoutubePlayerState();
+}
+
+class YoutubePlayerState extends State<YoutubePlayer> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ widet_test/lib/youtube/youtube_playlist.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class YoutubePlaylist extends StatefulWidget {
+  const YoutubePlaylist({super.key});
+
+  @override
+  State<YoutubePlaylist> createState() => YoutubePlaylistState();
+}
+
+class YoutubePlaylistState extends State<YoutubePlaylist> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
