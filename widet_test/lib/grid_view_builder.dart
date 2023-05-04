944330bb0de0595d865b70b31ed0066f0d925893
UwatakiYuichi <yuichi.uwataki@lanway.jp>
2023-05-04 13:09:41 +0900

    サムネイル作成箇所をメソッド化

--- widet_test/lib/grid_view_builder.dart
+++ widet_test/lib/grid_view_builder.dart
@@ -11,17 +11,28 @@ class _GriViewBuilderState extends State<GriViewBuilder> {
   @override
   Widget build(BuildContext context) {
     return GridView.builder(
+        // itemCount: 10,
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: 2, mainAxisSpacing: 10, crossAxisSpacing: 10),
         itemBuilder: (BuildContext context, int index) {
-          return const Image(
-            image:
-                NetworkImage('https://pbs.twimg.com/media/ECkqHryUYAECPPH.png'),
-            fit: BoxFit.cover,
-          );
+          return _createItem(index);
         });
   }
+
+  Widget _createItem(int index) {
+    return GestureDetector(
+      onTap: () {
+        print("no_${index}");
+      },
+      child: Image(
+        image: NetworkImage('https://pbs.twimg.com/media/ECkqHryUYAECPPH.png'),
+        fit: BoxFit.cover,
+      ),
+    );
+  }
 }
+
+
 
 // return GridView.builder(
 //       // scrollDirection: Axis.horizontal,
