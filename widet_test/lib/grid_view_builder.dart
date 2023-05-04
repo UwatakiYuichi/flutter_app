ff7c7bd77b3f0233a34728eeca003b4c822a60d3
UwatakiYuichi <yuichi.uwataki@lanway.jp>
2023-05-04 12:58:57 +0900

    サムネイル一覧を修正

--- widet_test/lib/grid_view_builder.dart
+++ widet_test/lib/grid_view_builder.dart
@@ -10,39 +10,18 @@ class GriViewBuilder extends StatefulWidget {
 class _GriViewBuilderState extends State<GriViewBuilder> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: SizedBox(
-          // GridViewの表示幅を固定
-          width: double.infinity,
-          child: GridView.count(
-            // 横1行あたりに表示するWidgetの数
-            crossAxisCount: 3,
-            // Widget間のスペース（左右）
-            crossAxisSpacing: 10,
-            // Widget間のスペース（上下）
-            mainAxisSpacing: 10,
-            // 全体の余白
-            padding: const EdgeInsets.symmetric(horizontal: 10),
-            children: [
-              // スプレッド演算子を使って画像を100個生成
-              for (int i = 0; i < 30; i++) ...[
-                GestureDetector(
-                  onTap: () {
-                    print('$iがタップされたんご!');
-                  },
-                  child: const Image(
-                    image: NetworkImage(
-                        'https://pbs.twimg.com/media/ECkqHryUYAECPPH.png'),
-                    fit: BoxFit.cover,
-                  ),
-                )
-              ],
-            ],
-          )),
-    );
+    return GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2, mainAxisSpacing: 10, crossAxisSpacing: 10),
+        itemBuilder: (BuildContext context, int index) {
+          return const Image(
+            image:
+                NetworkImage('https://pbs.twimg.com/media/ECkqHryUYAECPPH.png'),
+            fit: BoxFit.cover,
+          );
+        });
   }
 }
-
 
 // return GridView.builder(
 //       // scrollDirection: Axis.horizontal,
