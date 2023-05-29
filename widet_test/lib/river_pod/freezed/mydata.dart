298b978a77375609738b0a3ac5c2a13267454be1
UwatakiYuichi <yuichi.uwataki@lanway.jp>
2023-05-30 00:26:50 +0900

    youtubeのAPI処理を試験的に共通化してみる

--- widet_test/lib/river_pod/freezed/mydata.dart
+++ widet_test/lib/river_pod/freezed/mydata.dart
@@ -1,5 +1,6 @@
 import 'package:riverpod/riverpod.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:youtube_api/youtube_api.dart';
 
 part 'mydata.freezed.dart';
 
@@ -13,6 +14,12 @@ class MyData with _$MyData {
 // Providerで使うNotifier
 class MyDataStateNotifier extends StateNotifier<MyData> {
   MyDataStateNotifier() : super(const MyData());
+
+  final String apiKey = "AIzaSyCTYTR7JOPaR1aWKU1PNXPDblJK8h6cbCQ";
+  static String key = 'AIzaSyCTYTR7JOPaR1aWKU1PNXPDblJK8h6cbCQ';
+  String type = "channel";
+  YoutubeAPI youtube = new YoutubeAPI(key);
+
   // 値の書き換えは、copyWithで別インスタンスにする
   changeState(newValue) {
     // 下記はImmutableではNGのためエラー
@@ -30,5 +37,32 @@ class MyDataStateNotifier extends StateNotifier<MyData> {
     state = state.copyWith(isEnable: enable);
     // 変更の確認のためprintで情報を表示
     print("value:${state.isEnable}, hashCode:${state.hashCode}");
+  }
+
+  /**
+   * youtube動画一覧を取得する
+   *  query: 検索ワード
+   *  callback 通信後のコールバック処理
+   */
+  Future<void> searchYoutubeMovieList(String query, Function callback) async {
+    List<YouTubeVideo> videoResult = await youtube.search(
+      query,
+      order: 'relevance',
+      videoDuration: 'any',
+    );
+
+    state = state.copyWith(isEnable: true);
+
+    videoResult = await youtube.nextPage();
+
+    for (int i = 0; i < videoResult.length; i++) {
+      var youTubeVideo = videoResult[i];
+      print(youTubeVideo.url);
+      print(youTubeVideo.thumbnail.small.url);
+    }
+
+    callback();
+
+    state = state.copyWith(isEnable: false);
   }
 }
